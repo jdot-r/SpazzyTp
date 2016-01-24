d8fdb53858707054382628f5c0cac60819b2255f
PocketKiller <pocketkiller@imagicalmine.me>
2016-01-24 15:27:03 +0300

    Fixed Styling
    also why return true ?

--- src/Spazzy/Main.php
+++ src/Spazzy/Main.php
@@ -13,10 +13,10 @@ use pocketmine\level\particle\PortalParticle;
 use pocketmine\level\sound\EndermanTeleportSound;
 
 class Main extends PluginBase implements Listener {
-
-    public function onEnable(){
+	
+	public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
+	}
 
 	public function onLoad(){
 		$this->getLogger()->info("SpazzyTp §aenabled");
@@ -27,35 +27,31 @@ class Main extends PluginBase implements Listener {
 			case "pos":
 				if($sender instanceof Player){
 					$playerX = $sender->getX();
-                	$playerY = $sender->getY();
-                	$playerZ = $sender->getZ();
+                			$playerY = $sender->getY();
+                			$playerZ = $sender->getZ();
+                			
+                			$outX = round($playerX,1);
+                			$outY = round($playerY,1);
+                			$outZ = round($playerZ,1);
 
-                	$outX=round($playerX,1);
-                	$outY=round($playerY,1);
-                	$outZ=round($playerZ,1);
+                			$playerLevel = $sender->getLevel()->getName();
 
-                	$playerLevel = $sender->getLevel()->getName();
-
-                	$sender->sendMessage(TextFormat::BLUE . " x:" . TextFormat::GREEN . $outX . ", y:" . TextFormat::YELLOW . $outY . ", z:" . TextFormat::GREEN . $outZ . ". On: " . TextFormat::AQUA . $playerLevel);
+                			$sender->sendMessage(TextFormat::BLUE . " x:" . TextFormat::GREEN . $outX . ", y:" . TextFormat::YELLOW . $outY . ", z:" . TextFormat::GREEN . $outZ . ". On: " . TextFormat::AQUA . $playerLevel);
 					return true;
-				}
-
-				else{
+				}else{
 					$sender->sendMessage("§cThis command only works §ein§f-§egame.");
-            }
-		}
-	}    
-
-    public function onTeleport(EntityTeleportEvent $event){
+            			}
+			}
+		}    
+	 public function onTeleport(EntityTeleportEvent $event){
         $entity = $event->getEntity();
         $fizz = new EndermanTeleportSound($entity);
         $particle = new PortalParticle($entity);
         $entity->getLevel()->addSound($fizz);
         $entity->getLevel()->addParticle($particle);
-     }
+	 }
 	
-    public function onDisable(){
+	 public function onDisable(){
         $this->getLogger()->info("SpazzyTp §cdisabled.");
-        return true;
-	}
+	 }
 }
